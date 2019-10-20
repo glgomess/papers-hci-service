fdd431a8a62ed240331b1d9ec04ded900c20c392
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-10-20 18:34:39 -0300

    Get paper authors

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def homepage():
 @app.route('/papers')
 def getPapers():
   cursor = mysql.connection.cursor()
-  cursor.execute('SELECT paper_id, paper_year, paper_title FROM paper;')
+  cursor.execute('SELECT paper_id, paper_year, paper_title FROM paper')
   data = cursor.fetchall()
   cursor.close()
 
@@ -28,11 +28,37 @@ def getPapers():
 @app.route('/papers/<int:id>')
 def getPaperAbstractAndTitle(id):
   cursor = mysql.connection.cursor()
-  cursor.execute('SELECT paper_title, paper_abstract_PT FROM paper WHERE paper_id=' + str(id) + ';')
-  data = cursor.fetchall()
+  cursor.execute('SELECT paper_title, paper_abstract_PT FROM paper WHERE paper_id=' + str(id))
+  paperInfo = cursor.fetchone()
+
+  cursor.execute('SELECT person_id FROM author WHERE paper_id=' + str(id))
+  getAuthorName = 'SELECT person_name FROM person WHERE '
+  firstTime = True
+  for author in cursor:
+    authorId = author['person_id']
+    if firstTime:
+      getAuthorName = getAuthorName + 'person_id=' + str(authorId)
+      firstTime = False
+    else:
+      getAuthorName = getAuthorName + ' OR person_id=' + str(authorId)
+  
+  cursor.execute(getAuthorName)
+  authors = []
+  for person in cursor:
+    authorName = person['person_name']
+    authors.append(authorName)
+
+  response = {
+    'title': paperInfo['paper_title'],
+    'abstract_PT': paperInfo['paper_abstract_PT'],
+    'authors': authors
+  }
+  
   cursor.close()
 
-  return jsonify(data)
+  return jsonify(response)
+
+
 
 if __name__ == '__main__':
   app.run(port=5000, debug=TRUE)
